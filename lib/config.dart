519d6693b8c33a185cd6abc43af8d58b167784d6
nadaaputri <putriaqilah05@gmail.com>
2026-05-25 15:38:22 +0700

    set lokal

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,7 @@
 class AppConfig {
-  static const String baseUrl =
-      'https://pintukosbe-production-3cb3.up.railway.app';
+  //LOKAL
+  static const String baseUrl = 'http://localhost:8081';
+
+  //DEPLOY
+  //static const String baseUrl ='https://pintukosbe-production-3cb3.up.railway.app';
 }
